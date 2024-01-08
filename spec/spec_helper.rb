aedd00519897442a7231694302f7901517cdcb29
Anthea Yur <acyur6@gmail.com>
2024-01-07 18:30:42 -0600

    Refine user login instance data to spec_helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,20 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+def user_login_data
+  @user1 = User.create(first_name: 'Candy', last_name: 'Land', email: 'shawncarpenter.co@gmail.com', password: 'test')
+  visit '/login'
+  fill_in "email", with: "#{@user1.email}"
+  fill_in "password", with: "#{@user1.password}"
+  click_button 'log in'
+  email = ActionMailer::Base.deliveries.last
+  email_body = email.body.decoded
+  otp_code_match = email_body.match(/Your OTP code is: (\d+)/)
+  otp_code = otp_code_match[1]
+  fill_in 'otp', with: otp_code.to_i
+  click_button 'submit'
+end
+
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
