2cca4c81fcde168e1fefaa58b749de14eb9bc68c
Anthea Yur <acyur6@gmail.com>
2024-01-08 02:42:41 -0600

    Add formatting to display results

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,12 +7,6 @@ class LocationsController < ApplicationController
     @location_results = find_locations
   end
 
-  # #NOT SETUP -- THESE ARE PLACEHOLDERS AT THE MOMENT UNTIL BE IS SETUP
-  # def index
-  #   #refactor into service/poro/facade that also instantiates locations as objects
-  #   response = Faraday.get("http://127.0.0.1:3000/api/v0/locations")
-  #   @data = JSON.parse(response.body, symbolize_names: true)
-  # end
 
   # def show
   #   #refactor into service/poro/facade that also instantiates location as object
