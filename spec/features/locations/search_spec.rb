97b4db5a8ad7ca38187f493cd1c2a50c33073258
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-08 21:22:36 -0700

    VCR for locations index, search, users dashboard

--- spec/features/locations/search_spec.rb
+++ spec/features/locations/search_spec.rb
@@ -1,25 +1,35 @@
 require "rails_helper"
 
 RSpec.describe "Location Search Page", type: :feature do 
-  before :each do
-    user_login_data
-    user_select_loc_data
-    click_link "here"
-  end
+  # before :each do
+  #   user_login_data
+  #   user_select_loc_data
+  #   click_link "here"
+  # end
 
   describe "Search Page" do 
     it "has a form with fields for name and city" do
-      expect(current_path).to eq(locations_search_path)
-      expect(page).to have_content("What is the name and city of this third space?")
-      expect(page).to have_field('name')
-      expect(page).to have_field('city')
+      VCR.use_cassette("name and city") do 
+        user_login_data
+        user_select_loc_data
+        click_link "here"
+        expect(current_path).to eq(locations_search_path)
+        expect(page).to have_content("What is the name and city of this third space?")
+        expect(page).to have_field('name')
+        expect(page).to have_field('city')
+      end
     end
 
-    it "use can submit and entries and redirect to the search index" do
-      fill_in :name, with: "Five Watt"
-      fill_in :city, with: "Minneapolis"
-      click_button "submit"
-      expect(current_path).to eq("/locations")
+    it "user can submit and entries and redirect to the search index" do
+      VCR.use_cassette("fill in name and city") do 
+        user_login_data
+        user_select_loc_data
+        click_link "here"
+        fill_in :name, with: "Five Watt"
+        fill_in :city, with: "Minneapolis"
+        click_button "submit"
+        expect(current_path).to eq("/locations")
+      end
     end
   end
 end
