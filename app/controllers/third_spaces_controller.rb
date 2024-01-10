e0e62e3c6eb5e1fc7583c5fcf7be05336d080b9b
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-10 11:44:28 -0700

    Rewrite third space create to send in correct parameters

--- app/controllers/third_spaces_controller.rb
+++ app/controllers/third_spaces_controller.rb
@@ -8,24 +8,34 @@ class ThirdSpacesController < ApplicationController
   
   def create_third_space
     location = JSON.parse(params[:location_json], symbolize_names: true)
-    location = location.to_h.transform_keys(&:to_s)
     tags = params[:tags].map{|tag| tag.downcase.split.join("_")}
-    binding.pry
+    
     new_third_space_call(location, tags)
     redirect_to dashboard_path
   end
 
   private
-
-  def new_third_space_call(json_data, tags)
+  def new_third_space_call(location, tags)
     conn = Faraday.new(url: "http://localhost:3000") do |faraday| 
       faraday.headers['Content-Type'] = 'application/json'
-      faraday.body = json_data
     end
 
-    response = conn.post("/api/v1/third_spaces")
-    binding.pry
+    response = conn.post("/api/v1/third_spaces") do |req|
+      req.params['yelp_id'] = location[:yelp_id]
+      req.params['name'] = location[:name]
+      req.params['address'] = location[:address]
+      req.params['rating'] = location[:rating]
+      req.params['phone'] = location[:phone]
+      req.params['photos'] = "#{location[:photos]}"
+      req.params['lat'] = location[:lat]
+      req.params['lon'] = location[:lon]
+      req.params['price'] = location[:price]
+      req.params['hours'] = "#{location[:hours]}"
+      req.params['category'] = location[:category]
+      req.params['open_now'] = location[:open_now]
+      req.params['tags'] = "#{tags}"
+    end
+
     data = JSON.parse(response.body, symbolize_names: true)
   end
-
 end
