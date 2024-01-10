5781ccb68223ecb03dc5411c3940e1b12c1d6398
Anthea Yur <acyur6@gmail.com>
2024-01-09 19:30:50 -0600

    Created proper format for selection for New Third Space form

--- spec/features/locations/search_spec.rb
+++ spec/features/locations/search_spec.rb
@@ -1,18 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Location Search Page", type: :feature do 
-  # before :each do
-  #   user_login_data
-  #   user_select_loc_data
-  #   click_link "here"
-  # end
 
   describe "Search Page", :vcr do
     it "has a form with fields for name and city if there are not any locations available" do
       VCR.use_cassette("name and city") do 
         user_login_data
         user_select_loc_data
-        save_and_open_page
         click_link "here"
         expect(current_path).to eq(locations_search_path)
         expect(page).to have_content("What is the name and city of this third space?")
