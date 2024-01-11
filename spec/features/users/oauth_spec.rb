d1537c1f01bc581290ff4d96f0c4750f26384e4c
Nathan Trautenberg <Natester123@mac.com>
2024-01-11 12:40:24 -0700

    google oauth button is present. did not complete tests for validating oauth login.

--- spec/features/users/oauth_spec.rb
+++ spec/features/users/oauth_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "oauth", type: :feature do
   describe 'basic functionality' do
-    it 'can login with google' do
+    it 'has oauth login option' do
+      visit "/"
+      expect(page).to have_content("Continue with Google")
     end
+
   end
 end
