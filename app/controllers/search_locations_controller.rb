067259f1d95d0eb0dca21417b5d2fa080ed3c834
Anthea Yur <acyur6@gmail.com>
2024-01-11 01:33:25 -0600

    Changes made on geolocator for less room for error

--- app/controllers/search_locations_controller.rb
+++ app/controllers/search_locations_controller.rb
@@ -2,9 +2,10 @@ class SearchLocationsController < ApplicationController
   def create
     if current_user.search_location.nil? && params[:mood].present?
       geolocation = geocode_location(session[:lat], session[:lon])
-      geo_hash = geolocation.geolocation_parse(geolocation)
+      geo_hash = geolocation_parse(geolocation)
       search_location = current_user.build_search_location(geo_hash)
       search_location.save
+      search_location.update(mood: params[:mood])
     elsif current_user.search_location.nil?
       search_location = current_user.build_search_location(search_params)
       search_location.save
@@ -12,27 +13,29 @@ class SearchLocationsController < ApplicationController
       geolocation = geocode_location(session[:lat], session[:lon])
       geo_hash = geolocation_parse(geolocation)
       current_user.search_location.update(geo_hash)
+      current_user.search_location.update(mood: params[:mood])
     else
       current_user.search_location.update(search_params)
     end
     redirect_to dashboard_path
   end
 
+  
+  private
+  def search_params
+    params.permit(:city, :state)
+  end
+
   def geocode_location(lat, lon)
     results = Geocoder.search([lat, lon])
     results.first.address
   end
-
+  
   def geolocation_parse(geolocation)
     address_parts = geolocation.split(',').map(&:strip)
-    city = address_parts[3]
-    state = address_parts[4]
+    city = address_parts[4]
+    state = address_parts[6]
     geo_hash = { city: city, state: state }
     geo_hash
-  end
-
-  private
-  def search_params
-    params.permit(:city, :state)
   end
 end
