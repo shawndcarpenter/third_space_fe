8f767d5596ead6265285df4eef5c9a3f0ad5889e
Charles Ren <cren04@syr.edu>
2024-01-11 12:33:48 -0800

    test: sad path testing contact form

--- spec/features/contacts/new_spec.rb
+++ spec/features/contacts/new_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe 'Contact Support Form' do
       expect(page).to have_content('Your message has been submitted successfully')
     end
   end
+
+  it 'does not submit the contact form and renders the page again' do
+    VCR.use_cassette("contact form w redirect", record: :new_episodes) do
+      user_login_data
+      user_select_loc_data
+      visit new_contact_form_path
+      fill_in 'subject', with: ''
+      fill_in 'description', with: 'Test'
+
+      click_button 'submit'
+  
+      expect(page).to have_current_path(new_contact_form_path)
+      expect(page).to have_content('There was an error submitting your message. Please try again.')
+    end
+  end
 end
