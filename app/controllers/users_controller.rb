4aad24b85a96562f783cc10241d304674a7e050e
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-11 14:07:11 -0700

    Add logic for third space exists location search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
     end
 
     @search_location = @user.search_location
-    city = @search_location.city
-    state = @search_location.state
+    city = "Boulder"
+    state = "CO"
     @saved = SavedSpacesFacade.new(@user.id).spaces
     @saved_yelp_ids = saved_yelp_ids(@saved)
     spaces = ThirdSpacesFacade.new.spaces
