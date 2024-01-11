1ef726a9d383fb95cb5bb43ed85b77a728fd4e97
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-11 12:41:07 -0700

    Add new facades and services for reviews endpoints

---
+++ app/facades/location_reviews_facade.rb
@@ -0,0 +1,14 @@
+class LocationReviewsFacade
+  def initialize(yelp_id)
+    @yelp_id = yelp_id
+  end
+
+  def reviews
+    service = LocationReviewsService.new
+    json = service.get_location_reviews(@yelp_id)
+
+    @reviews = json.map do |review|
+      ReviewPoro.new(review[:attributes])
+    end
+  end
+end
--- app/services/third_spaces_service.rb
+++ app/services/third_spaces_service.rb
@@ -3,6 +3,21 @@ class ThirdSpacesService
     conn = Faraday.new(url: "http://localhost:3000")
   end
 
+  def create_third_space_reviews(review)
+    response = conn.post("/api/v1/third_spaces/#{review.yelp_id}/reviews") do |req|
+      req.params['yelp_id'] = review.yelp_id
+      req.params['name'] = review.name
+      req.params['rating'] = review.rating
+      req.params['text'] = review.text
+    end
+  end
+
+  def get_third_space_reviews(yelp_id)
+    response = conn.get("/api/v1/third_spaces/#{yelp_id}/reviews")
+
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
   def get_spaces_by_name(name)
     response = conn.get("/api/v1/third_spaces/search_by_name") do |req|
       req.params['name'] = name
