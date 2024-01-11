f9e03ccfa9d518f4f51dcd9463797da87b812910
Anthea Yur <acyur6@gmail.com>
2024-01-11 02:08:06 -0600

    Fix geolocation to convert state

--- app/controllers/search_locations_controller.rb
+++ app/controllers/search_locations_controller.rb
@@ -39,7 +39,63 @@ class SearchLocationsController < ApplicationController
     address_parts = geolocation.split(',').map(&:strip)
     city = address_parts[4]
     state = address_parts[6]
+    return geo_hash = { city: city, state: state_to_abv(state) } if state.length != 2
     geo_hash = { city: city, state: state }
-    geo_hash
+  end
+
+  def state_to_abv(state_name)
+    state_abbreviations = {
+                          'Alabama' => 'AL',
+                          'Alaska' => 'AK',
+                          'Arizona' => 'AZ',
+                          'Arkansas' => 'AR',
+                          'California' => 'CA',
+                          'Colorado' => 'CO',
+                          'Connecticut' => 'CT',
+                          'Delaware' => 'DE',
+                          'Florida' => 'FL',
+                          'Georgia' => 'GA',
+                          'Hawaii' => 'HI',
+                          'Idaho' => 'ID',
+                          'Illinois' => 'IL',
+                          'Indiana' => 'IN',
+                          'Iowa' => 'IA',
+                          'Kansas' => 'KS',
+                          'Kentucky' => 'KY',
+                          'Louisiana' => 'LA',
+                          'Maine' => 'ME',
+                          'Maryland' => 'MD',
+                          'Massachusetts' => 'MA',
+                          'Michigan' => 'MI',
+                          'Minnesota' => 'MN',
+                          'Mississippi' => 'MS',
+                          'Missouri' => 'MO',
+                          'Montana' => 'MT',
+                          'Nebraska' => 'NE',
+                          'Nevada' => 'NV',
+                          'New Hampshire' => 'NH',
+                          'New Jersey' => 'NJ',
+                          'New Mexico' => 'NM',
+                          'New York' => 'NY',
+                          'North Carolina' => 'NC',
+                          'North Dakota' => 'ND',
+                          'Ohio' => 'OH',
+                          'Oklahoma' => 'OK',
+                          'Oregon' => 'OR',
+                          'Pennsylvania' => 'PA',
+                          'Rhode Island' => 'RI',
+                          'South Carolina' => 'SC',
+                          'South Dakota' => 'SD',
+                          'Tennessee' => 'TN',
+                          'Texas' => 'TX',
+                          'Utah' => 'UT',
+                          'Vermont' => 'VT',
+                          'Virginia' => 'VA',
+                          'Washington' => 'WA',
+                          'West Virginia' => 'WV',
+                          'Wisconsin' => 'WI',
+                          'Wyoming' => 'WY'
+                        }
+    state_abbreviations[state_name]
   end
 end
