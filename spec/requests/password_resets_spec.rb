2e358d6ad5b8647f09fcc581339f1562a61f3d7c
Anthea Yur <acyur6@gmail.com>
2024-01-11 03:51:09 -0600

    Fix password reset tests

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "PasswordResets", type: :request do
+
   describe "GET /new" do
     it "returns http success" do
       get "/password/reset"
@@ -16,16 +17,36 @@ RSpec.describe "PasswordResets", type: :request do
   end
 
   describe "GET /edit" do
+    let(:user) do
+      User.create(
+        first_name: "Testing",
+        last_name: "Dude",
+        email: "to@example.org",
+        password: "password123",
+        password_confirmation: "password123"
+      )
+    end
+
     it "returns http success" do
-      get "/password/reset/edit"
+      get "/password/reset/edit", params: { token: user.signed_id(purpose: 'password_reset', expires_in: 15.minutes) }
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "PATCH /update" do
+    let(:user) do
+      User.create(
+        first_name: "Testing",
+        last_name: "Dude",
+        email: "to@example.org",
+        password: "password123",
+        password_confirmation: "password123"
+      )
+    end
+
     it "returns http success" do
-      patch "/password/reset/edit"
-      expect(response).to have_http_status(:success)
+      patch "/password/reset/edit", params: { token: user.signed_id(purpose: 'password_reset', expires_in: 15.minutes), user: { password: 'new_password', password_confirmation: 'new_password' } }
+      expect(response).to have_http_status(:redirect)
     end
   end
 
