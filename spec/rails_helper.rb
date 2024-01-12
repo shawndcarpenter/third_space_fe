03a9ccaaa45619f0d2cb3a971dc075765659e8b4
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-12 08:45:36 -0700

    Properly set up vcr to handle requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,8 +68,11 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  # config.configure_rspec_metadata!
-#   config.default_cassette_options = { re_record_interval: 365.days}
+  # config.filter_sensitive_data("<yelp>") {
+  #   Rails.application.credentials.yelp[:key]
+  # }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 365.days}
 end
 
 Shoulda::Matchers.configure do |config|
