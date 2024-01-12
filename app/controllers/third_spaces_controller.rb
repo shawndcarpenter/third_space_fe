a6db966bdc13520d53982df0226b21ba3008634f
Nathan Trautenberg <Natester123@mac.com>
2024-01-12 00:00:32 -0400

    successfully deletes third space on be server.

--- app/controllers/third_spaces_controller.rb
+++ app/controllers/third_spaces_controller.rb
@@ -74,9 +74,16 @@ class ThirdSpacesController < ApplicationController
 
   def destroy
     yelp_id = params[:id]
-    @space = find_third_space(yelp_id)
-    @space.destroy
-    redirect_to "/dashboard"
+    url = "http://localhost:3000/api/v1/third_spaces/#{yelp_id}"
+    if response.success?
+      response = Faraday.delete(url)
+      json_response = JSON.parse(response.body)
+      message = json_response['message']
+      flash[:success] = message
+      redirect_to "/dashboard"
+    else
+      flash[:error] = "Failed to delete record. HTTP status: #{response.status}"
+    end
   end
 
   private
