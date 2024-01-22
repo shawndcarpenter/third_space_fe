e60c0bfc0a2bf5fe592e503375ca3806e94c5457
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-22 15:12:42 -0700

    change credentials

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,15 @@ class UserMailer < ApplicationMailer
     require 'sendgrid-ruby'
 include SendGrid
 
+sendgrid_api_key = Rails.application.credentials.dig(:sendgrid, :key)
+
 from = Email.new(email: 'test@example.com')
 to = Email.new(email: 'test@example.com')
 subject = 'Sending with SendGrid is Fun'
 content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
 mail = Mail.new(from, subject, to, content)
 
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+sg = SendGrid::API.new(api_key: sendgrid_api_key)
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
