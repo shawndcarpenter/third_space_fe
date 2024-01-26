3efd8d50594618e1ffbdc75e2097cb6e126b8a41
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-26 09:15:39 -0700

    new sendgrid api key

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,9 +68,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  # config.filter_sensitive_data("<yelp>") {
-  #   Rails.application.credentials.yelp[:key]
-  # }
+  config.filter_sensitive_data('<SENDGRID_API_KEY>') { Rails.application.credentials.dig(:sendgrid, :key) }
   config.configure_rspec_metadata!
   config.default_cassette_options = { re_record_interval: 365.days}
 end
