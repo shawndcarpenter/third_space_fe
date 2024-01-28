5eff7272fc0b6a5f5b29aa978839676bd0a80d72
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-27 21:57:57 -0700

    96% coverage

--- spec/features/locations/index_spec.rb
+++ spec/features/locations/index_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "Find Locations", type: :feature do 
-  describe "Index" do 
-    before :each do
-      user_login_data
-      user_select_loc_data
-      save_and_open_page
+  # describe "Index" do 
+  #   before :each do
+  #     user_login_data
+  #     user_select_loc_data
+  #     save_and_open_page
       # click_link "here"
 
       # fill_in :name, with: "Five Watt"
       # fill_in :city, with: "Minneapolis"
       # click_button "submit"
-    end
+    # end
 
     
-    end
+    # end
   end
