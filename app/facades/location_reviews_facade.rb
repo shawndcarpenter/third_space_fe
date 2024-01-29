5b597c38e4be37dc5ac9454335617e04723b86cd
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-29 10:55:24 -0700

    handling nil for reviews

--- app/facades/location_reviews_facade.rb
+++ app/facades/location_reviews_facade.rb
@@ -6,8 +6,12 @@ class LocationReviewsFacade
   def reviews
     service = LocationReviewsService.new
     json = service.get_location_reviews(@yelp_id)
-    @reviews = json.map do |review|
-      ReviewPoro.new(review[:attributes])
+    if json
+      @reviews = json.map do |review|
+        ReviewPoro.new(review[:attributes])
+      end
+    else
+      @reviews = []
     end
   end
 end
