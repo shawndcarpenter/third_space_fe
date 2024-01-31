b1699beaa0022e3c74d9265f60d8803eb353f24a
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-30 19:38:25 -0700

    error handling I hope

--- app/facades/locations_facade.rb
+++ app/facades/locations_facade.rb
@@ -4,12 +4,25 @@ class LocationsFacade
     @city = city
   end
 
+  # def locations
+  #   service = LocationsService.new
+  #   json = service.get_location_results(@name, @city)
+
+  #   @search_results = json.map do |search_result|
+  #     SearchResult.new(search_result[:attributes])
+  #   end
+  # end
   def locations
     service = LocationsService.new
     json = service.get_location_results(@name, @city)
-
-    @search_results = json.map do |search_result|
-      SearchResult.new(search_result[:attributes])
+  
+    if json.is_a?(Array)
+      @search_results = json.map do |search_result|
+        SearchResult.new(search_result[:attributes])
+      end
+    else
+      @search_results = []
     end
   end
+  
 end
